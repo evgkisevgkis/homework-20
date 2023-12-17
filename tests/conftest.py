acf20fa122919337cd92641f4bb98d2d83969c1a
Johnny <evgkisevgkis@gmail.com>
2023-12-17 19:54:55 +0500

    редактирование мока DirectorDAO

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,13 +5,13 @@ from dao.director import DirectorDAO
 
 
 @pytest.fixture()
-def director_dao():
+def director_dao_fixture():
     director_dao = DirectorDAO(None)
     director = Director(id=1, name='Adam First')
     another_director = Director(id=2, name='Donald Love')
     director_dao.get_one = MagicMock(return_value=director)
     director_dao.get_all = MagicMock(return_value=[director, another_director])
     director_dao.create = MagicMock(return_value=Director(id=2))
-    director_dao.delete = MagicMock()
-    director_dao.update = MagicMock()
+    director_dao.delete = MagicMock(return_value='Deleted')
+    director_dao.update = MagicMock(return_value='Created')
     return director_dao
